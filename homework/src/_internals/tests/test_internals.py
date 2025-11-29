e8b5af99e0b31963215f96f438ef71041bbaae5b
AlejandroMontoyaV <alemova.1603@gmail.com>
2025-11-29 12:28:52 -0500

    Commit 3

--- homework/src/_internals/tests/test_internals.py
+++ homework/src/_internals/tests/test_internals.py
@@ -2,7 +2,10 @@ import subprocess
 import sys
 
 from ...wordcount import parse_args
+from ..count_words import count_words
+from ..preprocess_lines import preprocess_lines
 from ..read_all_lines import read_all_lines
+from ..split_into_words import split_into_words
 
 
 # Función test_parse_args
@@ -30,3 +33,28 @@ def test_read_all_lines():
         "Analytics refers to the systematic computational analysis of data" in line
         for line in lines
     )
+
+
+# Función test_preprocess_lines
+def test_preprocess_lines():
+    lines = [" Hello, World!  ", "This is a Test.", " Preprocess THIS line! "]
+    processed_lines = preprocess_lines(lines)
+    expected_lines = ["hello, world!", "this is a test.", "preprocess this line!"]
+    assert processed_lines == expected_lines
+
+
+# Función test_split_into_words
+def test_split_into_words():
+    lines = ["hello, world!", "this is a test."]
+    words = split_into_words(lines)
+    expected_words = ["hello", "world", "this", "is", "a", "test"]
+    assert words == expected_words
+
+
+# Función test_count_words
+def test_count_words():
+    words = ["hello", "world", "hello", "test"]
+    counter = count_words(words)
+    assert counter["hello"] == 2
+    assert counter["world"] == 1
+    assert counter["test"] == 1
