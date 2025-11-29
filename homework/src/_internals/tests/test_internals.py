2b98f27f2e3c2c09687cdb525e53a82ac5192041
AlejandroMontoyaV <alemova.1603@gmail.com>
2025-11-29 12:08:13 -0500

    commit 2

--- homework/src/_internals/tests/test_internals.py
+++ homework/src/_internals/tests/test_internals.py
@@ -2,8 +2,10 @@ import subprocess
 import sys
 
 from ...wordcount import parse_args
+from ..read_all_lines import read_all_lines
 
 
+# Función test_parse_args
 def test_parse_args():
     try:
         subprocess.run(
@@ -15,5 +17,16 @@ def test_parse_args():
 
     input_folder, output_folder = parse_args(["data/input", "data/output"])
 
-    assert input_folder == "data/input"
-    assert output_folder == "data/output"
+    assert input_folder == "data/input/"
+    assert output_folder == "data/output/"
+
+
+# Función test_read_all_lines
+def test_read_all_lines():
+    input_folder = "data/input/"
+    lines = read_all_lines(input_folder)
+    assert len(lines) > 0
+    assert any(
+        "Analytics refers to the systematic computational analysis of data" in line
+        for line in lines
+    )
--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,5 +1,10 @@
-import argparse
 import sys
+
+from ._internals.count_words import count_words
+from ._internals.preprocess_lines import preprocess_lines
+from ._internals.read_all_lines import read_all_lines
+from ._internals.split_into_words import split_into_words
+from ._internals.write_word_counts import write_word_counts
 
 
 def parse_args(args=None):
@@ -12,8 +17,33 @@ def parse_args(args=None):
         )
 
     input_folder, output_folder = args
+
+    # Asegurar slash final
+    if not input_folder.endswith("/"):
+        input_folder += "/"
+    if not output_folder.endswith("/"):
+        output_folder += "/"
+
     return input_folder, output_folder
 
 
 def main():
-    args = parse_args()
+    input_folder, output_folder = parse_args()
+    ## read all lines
+    all_lines = read_all_lines(input_folder)
+
+    ### preprocess lines
+    all_lines = preprocess_lines(all_lines)
+
+    ### split in words
+    words = split_into_words(all_lines)
+
+    ### count words
+    counter = count_words(words)
+
+    ### write word counts
+    write_word_counts(counter, output_folder)
+
+
+if __name__ == "__main__":
+    main()
