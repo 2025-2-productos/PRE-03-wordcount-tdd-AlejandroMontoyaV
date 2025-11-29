50307e9ea34e6029d23f2a41603799679f03ff72
AlejandroMontoyaV <alemova.1603@gmail.com>
2025-11-29 10:16:45 -0500

    primer commit

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,1 +1,19 @@
+import argparse
+import sys
 
+
+def parse_args(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+    if len(args) != 2:
+        raise ValueError(
+            "Must provide exactly 2 arguments: input_folder and output_folder"
+        )
+
+    input_folder, output_folder = args
+    return input_folder, output_folder
+
+
+def main():
+    args = parse_args()
